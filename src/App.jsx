e304021236694edea4f574c6e3b3e963d0e63926
salmanfaraj06 <salman.faraj06@gmail.com>
2024-06-03 00:44:00 +0530

    chore: Update import paths in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { Helmet } from "react-helmet";
 import Navbar from "./components/Navbar";
-import HeroSection from "components/Hero";
-import Testimonials from "components/Testimonials";
-import ServicesGrid from "components/ServicesGrid";
-import ContactUs from "components/ContactUs";
-import Footer from "components/Footer";
-import VisionMission from "components/VisionMission";
-import VMSmall from "components/VMSmall";
+import HeroSection from "./components/Hero";
+import Testimonials from "./components/Testimonials";
+import ServicesGrid from "./components/ServicesGrid";
+import ContactUs from "./components/ContactUs";
+import Footer from "./components/Footer";
+import VisionMission from "./components/VisionMission";
+import VMSmall from "./components/VMSmall";
 
 function App() {
   const [isLargeScreen, setIsLargeScreen] = useState(window.innerWidth >= 1024);
